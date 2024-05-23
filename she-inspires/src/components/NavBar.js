fd595c3bcaad53010fe453b346add2d20ee9e06e
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-05-23 18:46:30 +0200

    adding a logo to the navbarand sign-it and up sections

--- she-inspires/src/components/NavBar.js
+++ she-inspires/src/components/NavBar.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom'; 
+import logo from "../assets/logo.png";
+
+
 function NavBar() {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar expand="md">
             <Container>
-                <Navbar.Brand as={Link} to="/">She Inspires</Navbar.Brand>
+            <Navbar.Brand>
+          <img src={logo} alt="logo" height="60" />
+        </Navbar.Brand>
+
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto"> 
+                <Nav className="me-auto text-left"> 
                         <Nav.Link as={Link} to="/">Home</Nav.Link>
                         <Nav.Link as={Link} to="/stories">Stories</Nav.Link>
                         <Nav.Link as={Link} to="/resources">Resources</Nav.Link>
                         <Nav.Link as={Link} to="/community">Community</Nav.Link>
+                        <Nav.Link as={Link} to="/sign-in">Sign in</Nav.Link>
+                        <Nav.Link as={Link} to="/sign-up">Sign up</Nav.Link>
+                        
                     </Nav>
                 </Navbar.Collapse>
             </Container>
@@ -21,3 +30,4 @@ function NavBar() {
 }
 
 export default NavBar;
+ 
