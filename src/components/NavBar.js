9ba1d8f72148b895aa23a1707321159a38d47641
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-06-18 09:44:45 +0200

    adiing  access and refresh tokens

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
