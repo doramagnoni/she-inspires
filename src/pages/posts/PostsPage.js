2dfc4857a4cb461eda95546ce78ad8362aef2dc6
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-06-25 01:26:19 +0200

    creating utils and adding props for the infinite scroll in the posts page

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -12,6 +12,9 @@ import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 
 import NoResults from "../../assets/no-results.png";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
+
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -60,12 +63,16 @@ function PostsPage({ message, filter = "" }) {
         </Form>
         {hasLoaded ? (
           <>
-            {posts.results.length ? (
-              posts.results.map((post) => (
-              <div key={post.id} className={styles.PostWrapper}>
+          {posts.results.length ? (
+            <InfiniteScroll
+              children={posts.results.map((post) => (
                 <Post key={post.id} {...post} setPosts={setPosts} />
-              </div>             
-              ))
+              ))}
+              dataLength={posts.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!posts.next}
+              next={() => fetchMoreData(posts, setPosts)}
+            />
             ) : (
               <Container className={appStyles.Content}>
                 <Asset src={NoResults} message={message} />
