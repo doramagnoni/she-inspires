68658b2e8e94589cb4ffa5ed4ddc51082989a67a
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-06-25 16:24:56 +0200

    adding the infinite scroll to the comments

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -11,6 +11,9 @@ import Comment from "../comments/Comment";
 
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostPage() {
   const { id } = useParams();
@@ -55,19 +58,26 @@ function PostPage() {
               "Comments"
             ) : null}
             {comments.results.length ? (
-              comments.results.map((comment) => (
-                <Comment
-                  key={comment.id}
-                  {...comment}
-                  setPost={setPost}
-                  setComments={setComments}
-                />
-              ))
-            ) : currentUser ? (
-              <span>No comments yet, be the first to comment!</span>
-            ) : (
-              <span>No comments... yet</span>
-            )}
+             <InfiniteScroll
+             children={comments.results.map((comment) => (
+               <Comment
+                 key={comment.id}
+                 {...comment}
+                 setPost={setPost}
+                 setComments={setComments}
+               />
+             ))}
+             dataLength={comments.results.length}
+             loader={<Asset spinner />}
+             hasMore={!!comments.next}
+             next={() => fetchMoreData(comments, setComments)}
+           />
+         ) : currentUser ? (
+           <span>No comments yet, be the first to comment!</span>
+         ) : (
+           <span>No comments... yet</span>
+         )}
+            
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
