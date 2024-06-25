3178230cb5355b2dc5a4925b21ead9e4c30e23e7
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-06-25 15:32:03 +0200

    integrating a Comment Component Dropdown Menu and styling the comments section

---
+++ src/pages/comments/Comment.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+import { MoreDropdown } from "../../components/MoreDropdown";
+import styles from "../../styles/Comment.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { axiosRes } from "../../api/axiosDefaults";
+
+const Comment = (props) => {
+  const {
+    profile_id,
+    profile_image,
+    owner,
+    updated_at,
+    content,
+    id,
+    setPost,
+    setComments,
+  } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/comments/${id}/`);
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count - 1,
+          },
+        ],
+      }));
+
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.filter((comment) => comment.id !== id),
+      }));
+    } catch (err) {}
+  };
+
+  return (
+    <div className={styles.Comment}>
+      <hr />
+      <div className={styles.CommentBody}>
+        <Link to={`/profiles/${profile_id}`}>
+          <Avatar src={profile_image} />
+        </Link>
+        <div className={styles.CommentContent}>
+          <div className={styles.CommentHeader}>
+            <span className={styles.Owner}>{owner}</span>
+            <span className={styles.Date}>{updated_at}</span>
+          </div>
+          <p className={styles.CommentText}>{content}</p>
+        </div>
+        {is_owner && (
+          <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Comment;
