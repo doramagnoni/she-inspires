92183fdbab007ff142acd01fae22a6a8dcfbb82e
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-07-27 12:42:24 +0200

    fixing an issue with the use of navigate push in the currentUserProvider causing an error while users are signin in

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -34,7 +34,7 @@ export const CurrentUserProvider = ({ children }) => {
         } catch (err) {
           setCurrentUser((prevCurrentUser) => {
             if (prevCurrentUser) {
-             navigate.push("/signin");
+              navigate("/signin"); 
             }
             return null;
           });
@@ -56,7 +56,7 @@ export const CurrentUserProvider = ({ children }) => {
           } catch (err) {
             setCurrentUser((prevCurrentUser) => {
               if (prevCurrentUser) {
-                navigate.push("/signin");
+                navigate("/signin"); 
               }
               return null;
             });
