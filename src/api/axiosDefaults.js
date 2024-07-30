f6f02127352071b88ac405847c0d98cd18989ec2
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-07-30 09:55:29 +0200

    adding a JWT refresh token to axious

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,52 @@
-import axios from "axios";
+import axios from 'axios';
 
-axios.defaults.baseURL = "https://sheapi-001672ab3b00.herokuapp.com/";
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.baseURL = 'https://sheapi-001672ab3b00.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
+
+async function refreshToken() {
+    try {
+      const response = await axios.post('/dj-rest-auth/token/refresh/', {
+        refresh_token: localStorage.getItem('refreshToken')
+      });
+      return response.data.access_token; // Adjust according to your response structure
+    } catch (error) {
+      console.error('Failed to refresh token:', error);
+      throw error;
+    }
+  }
+
+axiosReq.interceptors.request.use(
+  (config) => {
+    const storedAccessToken = localStorage.getItem('accessToken');
+    if (storedAccessToken) {
+      config.headers.Authorization = `Bearer ${storedAccessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosReq.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const newAccessToken = await refreshToken();
+        localStorage.setItem('accessToken', newAccessToken);
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return axiosReq(originalRequest);
+      } catch (err) {
+        console.error('Failed to refresh token:', err);
+        // Handle refresh token failure (e.g., logout user)
+        // Example: window.location.href = '/login';
+      }
+    }
+    return Promise.reject(error);
+  }
+);
